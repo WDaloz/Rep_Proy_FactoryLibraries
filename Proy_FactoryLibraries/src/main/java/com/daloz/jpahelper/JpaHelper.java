3a06168bd279b2c7d4f18ba467154f3e744e4f29
Sebastian <cristhianp00@gmail.com>
2016-09-13 21:09:54 -0500

    Creando nuevo helper

--- Proy_FactoryLibraries/src/main/java/com/daloz/jpahelper/JpaHelper.java
+++ Proy_FactoryLibraries/src/main/java/com/daloz/jpahelper/JpaHelper.java
@@ -8,18 +8,35 @@ public class JpaHelper
 {
 	@SuppressWarnings("unused")
 	private String queryHql, declared, condition, nameClassJoin, nameAlliedClassJoin, nameClassMaster, nameAlliedClassMaster;
-	
-	private Object object;
+	private Boolean isJoin = false;
+	private Object objecMaster;
 
 	private List<BagItem> bagItems;
 	
 	public JpaHelper(String condition, Object object)
 	{
-		this.queryHql = condition;
-		this.object = object;
+		this.queryHql = condition.trim();
+		this.objecMaster = object;
 		
+		getIsJoin();
 		splitCondition();
 		populateNameAttributesAndValueAttributes();
+	}
+	
+	private void getIsJoin()
+	{
+		declared = queryHql.split("WHERE")[0];
+		
+		String[] arrayDeclared = declared.split(" ");
+		
+		for (int i = 0; i < arrayDeclared.length; i++)
+		{
+			if(arrayDeclared[i].equals("JOIN"))
+			{
+				isJoin = true;
+				break;
+			}
+		}
 	}
 
 	private void splitCondition()
@@ -27,8 +44,11 @@ public class JpaHelper
 		declared = queryHql.split("WHERE")[0];
 		condition = queryHql.split("WHERE")[1];
 
-		nameClassJoin = declared.split("JOIN")[1].trim().split(" ")[0].substring(2);
-		nameAlliedClassJoin = declared.split("JOIN")[1].trim().split(" ")[1];
+		if(isJoin)
+		{
+			nameClassJoin = declared.split("JOIN")[1].trim().split(" ")[0].substring(2);
+			nameAlliedClassJoin = declared.split("JOIN")[1].trim().split(" ")[1];
+		}
 
 		nameClassMaster = declared.split("FROM")[1].trim().split(" ")[0];
 		nameAlliedClassMaster = declared.split("FROM")[0].trim().split(" ")[1];
@@ -44,42 +64,66 @@ public class JpaHelper
 			//Instanciamos las lista.
 			bagItems = new ArrayList<>();
 			
+			//Recorremos el arreglo de criterios.
 			for (int i = 0; i < arrayCriteria.length; i++)
 			{
 
-				if (arrayCriteria[i].length() > 1 && arrayCriteria[i].substring(0, 2).equals(nameAlliedClassMaster + "."))
+				Integer lengthCriteria = arrayCriteria[i].length();
+				
+				if ( lengthCriteria > 1 && arrayCriteria[i].substring(0, 2).equals(nameAlliedClassMaster+"."))
 				{
+					//Instanciamos la bolsa
 					BagItem bagItem = new BagItem();
 					
-					// Ingresando nombre del atributo
-					bagItem.setNameAttribute(arrayCriteria[i].substring(2));
+					// capturamos nombre del atributo
+					String nameAttribute = arrayCriteria[i].substring(2);
 
-					Field field = object.getClass().getDeclaredField(arrayCriteria[i].substring(2));
+					//Obtenemos el atributo mediante su nombre
+					Field field = objecMaster.getClass().getDeclaredField(nameAttribute);
 					field.setAccessible(true);
 
-					bagItem.setValueAttribute(field.get(object));
+					Object objectValueAttribute = field.get(objecMaster);
 					
-					//Añadiendo a la lista
+					//Inyectando datos al objeto.
+					bagItem.setNameAttribute(nameAttribute);
+					bagItem.setValueAttribute(objectValueAttribute);
+					
+					//Añadiendo objeto a la lista.
 					bagItems.add(bagItem);
 				} 
-				else if (arrayCriteria[i].length() > 1 && arrayCriteria[i].substring(0, 2).equals(nameAlliedClassJoin + "."))
+				else if (isJoin == true && lengthCriteria > 1 && arrayCriteria[i].substring(0, 2).equals(nameAlliedClassJoin+"."))
 				{
+					//Instanciamos la bolsa
 					BagItem bagItem = new BagItem();
 					
-					// Ingresando nombre del atributo
-					bagItem.setNameAttribute(arrayCriteria[i].substring(2));
+					// capturamos nombre del atributo
+					String nameAttribute = arrayCriteria[i].substring(2);
 					
-					Field field = object.getClass().getDeclaredField(nameClassJoin);
+					//Obtenemos el atributo mediante su nombre
+					Field field = objecMaster.getClass().getDeclaredField(nameClassJoin);
 					field.setAccessible(true);
 					
-					Object objectj = field.get(object);
+					//Obtenemos el valor del atributo con la instancia del objeto maestro.
+					Object objectJoin = field.get(objecMaster);
 					
-					Field fieldj =objectj.getClass().getDeclaredField(arrayCriteria[i].substring(2));
-					fieldj.setAccessible(true);
+					//Objeto que contendra el valor del atributo del objetoJoin
+					Object objectValueAttribute = null;
 					
-					bagItem.setValueAttribute(fieldj.get(objectj));
 					
-					//Añadiendo a la lista
+					if(objectJoin != null)
+					{
+						Field fieldj =objectJoin.getClass().getDeclaredField(arrayCriteria[i].substring(2));
+						fieldj.setAccessible(true);
+						
+						objectValueAttribute = fieldj.get(objectJoin);
+					}
+					
+					
+					//Inyectando datos al objeto.
+					bagItem.setNameAttribute(nameAttribute);
+					bagItem.setValueAttribute(objectValueAttribute);
+					
+					//Añadiendo objeto a la lista.
 					bagItems.add(bagItem);
 				}
 
@@ -91,8 +135,23 @@ public class JpaHelper
 		}
 	}
 
-	public List<BagItem> getBagItems()
+	public List<BagItem> getBagItems(Boolean conditionAttributeIsNotNutll)
 	{
-		return bagItems;
+		if(conditionAttributeIsNotNutll)
+		{
+			for (int i = 0; i < bagItems.size(); i++)
+			{
+				if(bagItems.get(i).getValueAttribute() == null)
+				{
+					bagItems.remove(i);
+				}
+			}
+			
+			return bagItems;
+		}
+		else
+		{
+			return bagItems;
+		}
 	}
 }
--- Proy_FactoryLibraries/src/test/java/com/test/jpahelper/Probando.java
+++ Proy_FactoryLibraries/src/test/java/com/test/jpahelper/Probando.java
@@ -1,9 +1,9 @@
 package com.test.jpahelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 
+import com.daloz.jpahelper.BagItem;
 import com.daloz.jpahelper.JpaHelper;
 import com.daloz.model.EmployeeDTO;
 import com.daloz.model.UserDTO;
@@ -12,12 +12,7 @@ public class Probando
 {
 	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
 	{
-		
-		List<HashMap<String, Object>> hashMaps = new ArrayList<>();
-	
-		
-		
-		
+				
 		EmployeeDTO employeeDTO = new EmployeeDTO();
 		employeeDTO.setId(new Long(2));
 		employeeDTO.setName("Cristhian");
@@ -25,8 +20,17 @@ public class Probando
 		UserDTO user = new UserDTO();
 		user.setCode("USER001");
 		
-		employeeDTO.setUserDTO(user);
+//		employeeDTO.setUserDTO(user);
 		
-		new JpaHelper("SELECT x FROM EmployeeDTO x WHERE x.id = :id AND x.name = :name", employeeDTO);
+		
+		String conditionA = "   SELECT x FROM EmployeeDTO INNER JOIN x.userDTO u WHERE x.id = :id AND u.code = :code AND x.name = :name ";
+		String conditionB = "SELECT e FROM EmployeeDTO e WHERE e.name = :name ";
+		
+		List<BagItem> b =new JpaHelper(conditionA, employeeDTO).getBagItems(false);
+		
+		for (int i = 0; i < b.size(); i++)
+		{
+			System.out.println(b.get(i).getNameAttribute() +" --- "+b.get(i).getValueAttribute());
+		}
 	}
 }
